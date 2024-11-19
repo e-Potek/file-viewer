9890a189da8f6b57c6e810d9233de6d33f5de3a0
Cazacu Vlad-Gabriel <gabrielcazacu96@gmail.com>
2024-11-19 15:20:17 +0100

    Publish version 0.1.4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import * as pdfjsLib from 'pdfjs-dist';
+import * as pdfjs from 'pdfjs-dist';
 import FileViewer from './FileViewer';
 
 const setWorkerPort = workerPort => {
-  pdfjsLib.GlobalWorkerOptions.workerPort = workerPort;
+  pdfjs.GlobalWorkerOptions.workerPort = workerPort;
 };
 
 const setWorkerSrc = workerSrc => {
-  pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
+  pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 };
 
-export { FileViewer, setWorkerSrc, setWorkerPort };
+export { FileViewer, setWorkerSrc, setWorkerPort, pdfjs };
 
 export default FileViewer;
